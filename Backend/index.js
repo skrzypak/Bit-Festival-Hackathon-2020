7fa6e33091aab0a86f0bc75471b7d0ac80341812
skrzypak <skrzypczykmail@gmail.com>
2020-12-05 10:14:55 +0100

    simply get

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const GovStats = require('./GovStats')
 const WhoInformation = require('./WhoInformation');
 const News = require('./News');
+const Database = require('./Database');
 const app = express()
 const PORT = process.env.PORT || 8080;
+const db = new Database()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -16,33 +18,46 @@ app.get('/news',async (req, res) => {
 
 app.get('/who', (req, res) => {
   new WhoInformation().updateTable()
-  res.send("Done")
+  res.send("")
 })
 
-app.get('/getCSVFromGOV',async (req, res) => {
-  //let data = await new GovStats().getCSVDataProvince()
-  let data2 = await new GovStats().getCSVDataCounties();
-  // let countiesCsvUri = await new GovStats().getCSVFromGOV('https://www.gov.pl/web/koronawirus/mapa-zarazen-koronawirusem-sars-cov-2-powiaty');
-  // console.log("elo: ", countiesCsvUri)
-
-  res.send(data2)
+app.get('/refreshCsvDataProvinceDB',async (req, res) => {
+  await new GovStats().getCSVDataProvince()
+  res.send("")
 })
 
-app.get('/getNationalRestrictions', async (req, res) => {
+app.get('/refreshCsvdataCountiesDB',async (req, res) => {
+  await new GovStats().getCSVDataCounties();
+  res.send("")
+})
+
+app.get('/refreshNationalRestrictionsDB', async (req, res) => {
   let date = await new GovStats().getNationalRestrictionsDate();
-
-  // TODO
   date = date[6] + date[7] + date[8] + date[9] + '-' + date[3] + date[4] + '-' + date[0] + date[1]
   let n = await new GovStats().existsNationalRestrictionsDate(date);
-
   if(n === false) {
     let obj = await new GovStats().getNationalRestrictionsContent();
     await new GovStats().pushNationalRestrictions(obj, date);
-
     res.send(true)
-  } else console.log("EXISTS")
+  } else res.send(false)
+
 });
 
+/**
+ * Getting data from database query
+ */
+app.get('/db', async (req, res) => {
+  let query = req._parsedUrl.query;
+  query = query.replace('query=','')
+  let data = await new Database().get(query);
+  console.log(data);
+  res.send(data)
+});
+
+app.get('test', async (req, res) => {
+   //`select * from nationalRestrictions`
+   //res.send(0)
+}) 
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
