fccc62a08aa001e5d731769a8e32c496f7e967b0
Łukasz Goleniec <30418440+Flashrow@users.noreply.github.com>
2020-12-05 16:50:01 +0100

    cities

--- Flutter/corona_info/lib/screens/statistics/statisticsScreen.dart
+++ Flutter/corona_info/lib/screens/statistics/statisticsScreen.dart
@@ -7,77 +7,14 @@ import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
 import 'package:flutter_socket_io/flutter_socket_io.dart';
-import 'package:flutter_socket_io/socket_io_manager.dart';
-import 'package:http/http.dart' as http;
 
-class ProvinceFechData {
-  final String id;
-  final String province;
-  final String date_;
-  final String activeNumber;
-  final String activeNumberPer10k;
-  final String deaths;
-  final String deathsWithoutIll;
-  final String deathsWithIll;
 
-  ProvinceFechData(
-      this.id,
-      this.province,
-      this.date_,
-      this.activeNumber,
-      this.activeNumberPer10k,
-      this.deaths,
-      this.deathsWithoutIll,
-      this.deathsWithIll) {}
 
-  ProvinceFechData.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        province = json['province'],
-        date_ = json['date_'],
-        activeNumber = json['activeNumber'],
-        activeNumberPer10k = json['activeNumberPer10k'],
-        deaths = json['deaths'],
-        deathsWithoutIll = json['deathsWithoutIll'],
-        deathsWithIll = json['deathsWithIll'];
-}
-
-class CountiesFechData {
-  final String id;
-  final String province;
-  final String city;
-  final String date_;
-  final String activeNumber;
-  final String activeNumberPer10k;
-  final String deaths;
-  final String deathsWithoutIll;
-  final String deathsWithIll;
-
-  CountiesFechData(
-      this.id,
-      this.province,
-      this.city,
-      this.date_,
-      this.activeNumber,
-      this.activeNumberPer10k,
-      this.deaths,
-      this.deathsWithoutIll,
-      this.deathsWithIll) {}
-
-  CountiesFechData.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        province = json['province'],
-        city = json['city'],
-        date_ = json['date_'],
-        activeNumber = json['activeNumber'],
-        activeNumberPer10k = json['activeNumberPer10k'],
-        deaths = json['deaths'],
-        deathsWithoutIll = json['deathsWithoutIll'],
-        deathsWithIll = json['deathsWithIll'];
-}
 
 class StatisticsScreen extends StatelessWidget {
   SocketIO socketIO;
   List<String> messages;
+  
 
   List<List<String>> statsValues = [
     ["Statystyki", "Suma", "Średnia\n(dzienna)"],
@@ -87,62 +24,6 @@ class StatisticsScreen extends StatelessWidget {
     ["Aktywne", "543 123", "-"]
   ];
 
-  StatisticsScreen() {
-    //initProvinces();
-    initCounties('lubelskie');
-  }
-
-  Future<void> initProvinces() async {
-    var url = 'http://89.74.231.9:8080'; //
-    var response = await http.get(url + '/provinces?'); //
-    if (response.statusCode == 200) {
-      List<dynamic> values = new List<dynamic>();
-      List<Map<String, dynamic>> _homeFechData =
-          new List<Map<String, dynamic>>();
-      values = json.decode(response.body);
-      if (values.length > 0) {
-        for (int i = 0; i < values.length; i++) {
-          if (values[i] != null) {
-            Map<String, dynamic> map = values[i];
-
-            //TODO ... province
-            print(map);
-            // ......
-
-            // todo lista wojewodztw
-
-          }
-        }
-      }
-      return _homeFechData;
-    }
-  }
-
-  Future<void> initCounties(name) async {
-    var url = 'http://89.74.231.9:8080'; //
-    var response = await http.get(url + '/counties/' + name + '?'); //
-    if (response.statusCode == 200) {
-      List<dynamic> values = new List<dynamic>();
-      List<Map<String, dynamic>> _homeFechData =
-          new List<Map<String, dynamic>>();
-      values = json.decode(response.body);
-      if (values.length > 0) {
-        for (int i = 0; i < values.length; i++) {
-          if (values[i] != null) {
-            Map<String, dynamic> map = values[i];
-
-            //TODO ... province counties
-            print(map);
-            // ......
-
-            // Todo lista powaitow
-
-          }
-        }
-      }
-      return _homeFechData;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
--- Flutter/corona_info/lib/screens/statistics/statisticsSearch.dart
+++ Flutter/corona_info/lib/screens/statistics/statisticsSearch.dart
@@ -1,9 +1,170 @@
+import 'dart:convert';
+
 import 'package:corona_info/main.dart';
 import 'package:corona_info/widgets/AppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+import 'package:http/http.dart' as http;
 
-class StatisticSearch extends StatelessWidget {
-  StatisticSearch({Key key}) : super(key: key);
+class ProvinceFechData {
+  final String id;
+  final String province;
+  final String date_;
+  final String activeNumber;
+  final String activeNumberPer10k;
+  final String deaths;
+  final String deathsWithoutIll;
+  final String deathsWithIll;
+
+  ProvinceFechData(
+      this.id,
+      this.province,
+      this.date_,
+      this.activeNumber,
+      this.activeNumberPer10k,
+      this.deaths,
+      this.deathsWithoutIll,
+      this.deathsWithIll) {}
+
+  ProvinceFechData.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        province = json['province'],
+        date_ = json['date_'],
+        activeNumber = json['activeNumber'],
+        activeNumberPer10k = json['activeNumberPer10k'],
+        deaths = json['deaths'],
+        deathsWithoutIll = json['deathsWithoutIll'],
+        deathsWithIll = json['deathsWithIll'];
+}
+
+class CountiesFechData {
+  int id;
+  String province;
+  String city;
+  String date_;
+  int activeNumber;
+  double activeNumberPer10k;
+  int deaths;
+  int deathsWithoutIll;
+  int deathsWithIll;
+
+  CountiesFechData(
+      {this.id,
+      this.province,
+      this.city,
+      this.date_,
+      this.activeNumber,
+      this.activeNumberPer10k,
+      this.deaths,
+      this.deathsWithoutIll,
+      this.deathsWithIll});
+
+  factory CountiesFechData.fromJson(Map<String, dynamic> json) {
+    return CountiesFechData(
+        id: json['id'],
+        province: json['province'],
+        city: json['city'],
+        date_: json['date_'],
+        activeNumber: json['activeNumber'],
+        activeNumberPer10k: json['activeNumberPer10k'],
+        deaths: json['deaths'],
+        deathsWithoutIll: json['deathsWithoutIll'],
+        deathsWithIll: json['deathsWithIll']);
+  }
+
+  CountiesFechData getFromJson(Map<String, dynamic> json) {
+    CountiesFechData cfd = new CountiesFechData();
+    cfd.id = json['id'];
+    cfd.province = json['province'];
+    cfd.city = json['city'];
+    cfd.date_ = json['date_'];
+    cfd.activeNumber = json['activeNumber'];
+    cfd.activeNumberPer10k = json['activeNumberPer10k'];
+    cfd.deaths = json['deaths'];
+    cfd.deathsWithIll = json['deathsWithIll'];
+    cfd.deathsWithoutIll = json['deathsWithoutIll'];
+    return cfd;
+  }
+}
+
+class StatisticSearch extends StatefulWidget {
+  @override
+  _StatisticSearchState createState() => _StatisticSearchState();
+}
+
+class _StatisticSearchState extends State<StatisticSearch> {
+  Future<List<String>> cities;
+
+  StatisticsScreen({Key key}) {
+    //initProvinces();
+  }
+
+  @override
+  void initState() {
+    cities = initCounties('lubelskie');
+    super.initState();
+    print("halo");
+    //initCounties('lubelskie');
+  }
+
+  // Future<List<String>> initProvinces() async {
+  //   List<String> tempString = new List();
+  //   var url = 'http://89.74.231.9:8080'; //
+  //   var response = await http.get(url + '/provinces?'); //
+  //   if (response.statusCode == 200) {
+  //     List<dynamic> values = new List<dynamic>();
+  //     List<Map<String, dynamic>> _homeFechData =
+  //         new List<Map<String, dynamic>>();
+  //     values = json.decode(response.body);
+  //     if (values.length > 0) {
+  //       for (int i = 0; i < values.length; i++) {
+  //         if (values[i] != null) {
+  //           Map<String, dynamic> map = values[i];
+
+  //           //TODO ... province
+  //           //print(map);
+  //           ProvinceFechData province = ProvinceFechData.fromJson(map);
+  //           // print(province.id);
+
+  //           // ......
+
+  //           // todo lista wojewodztw
+
+  //         }
+  //       }
+  //     }
+  //     return _homeFechData;
+  //   }
+  // }
+
+  Future<List<String>> initCounties(name) async {
+    List<String> tempString = new List();
+    var url = 'http://89.74.231.9:8080'; //
+    var response = await http.get(url + '/counties/' + name + '?'); //
+    if (response.statusCode == 200) {
+      List<dynamic> values = new List<dynamic>();
+      List<Map<String, dynamic>> _homeFechData =
+          new List<Map<String, dynamic>>();
+      values = json.decode(response.body);
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+
+            //TODO ... province counties
+            //print(map);
+            // ......
+            //print(map["city"]);
+            tempString.add(map["city"]);
+            // print(cities);
+            // Todo lista powaitow
+
+          }
+        }
+      }
+      return tempString.toList();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +176,23 @@ class StatisticSearch extends StatelessWidget {
             children: <Widget>[
               Container(child: MyAppBar("Obostrzenia")),
               Container(child: TextField()),
-              singleRow('Polska'),
-              singleRow('Powiat1'),
-              singleRow('Powiat2'),
-              singleRow('Powiat3'),
-              singleRow('Powiat4'),
-              singleRow('Powiat5'),
-              singleRow('Powiat6'),
-              singleRow('Powiat7'),
-              singleRow('Powiat8'),
+              FutureBuilder<List<String>>(
+                future: cities,
+                builder: (context, snapshot) {
+                  return Expanded(
+                                      child: Container(
+                      child: ListView(
+                        children: [
+                          Column(
+                              children: snapshot.data
+                                  .map((city) => singleRow(city))
+                                  .toList()),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ],
           ),
         ),
@@ -37,7 +206,8 @@ class StatisticSearch extends StatelessWidget {
       child: Expanded(
         flex: 3,
         child: Container(
-          decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
+          decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey))),
           height: 48,
           child: Center(
             child: Align(alignment: Alignment.centerLeft, child: Text(data)),
