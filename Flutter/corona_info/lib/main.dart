ad821c9d196468d1a6cc950f1ddbe7e68daf0fdc
Łukasz Goleniec <30418440+Flashrow@users.noreply.github.com>
2020-12-05 04:16:45 +0100

    main screen, svg handle

--- Flutter/corona_info/lib/main.dart
+++ Flutter/corona_info/lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeScreen(),
-      //home: RestrictionScreen(),
     );
   }
 }
